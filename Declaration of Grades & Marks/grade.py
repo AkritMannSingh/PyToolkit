360f62055214c6a15d236fe6faef371781f1689c
Akrit Mann Singh <akritmannsingh@gmail.com>
2026-01-22 15:41:19 +0530

    Update grade.py

--- Declaration of Grades & Marks/grade.py
+++ Declaration of Grades & Marks/grade.py
@@ -1,1 +1,21 @@
+print("Grade Computation System\n\n")
+
+score = int(input("Enter the student's score(0-100):"))
+
+if (score >= 90):
+  grade = 'A'
+
+elif (score >= 80):
+  grade = 'B'
+
+elif (score >= 70):
+  grade = 'C'
+
+elif (score >= 60):
+  grade = 'D'
+
+else:
+  grade = 'F'
+
+print("The student's grade is:", grade)
 
