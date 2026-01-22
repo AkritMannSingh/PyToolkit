190d1c1def00747bb0eceb94d384b28c3c010354
Akrit Mann Singh <akritmannsingh@gmail.com>
2026-01-22 16:10:56 +0530

    Update char_identifier.py

--- Letter Detective/char_identifier.py
+++ Letter Detective/char_identifier.py
@@ -1,1 +1,17 @@
+print("Welcome to Character Identifier 🔤")
 
+ch = input("Enter a character: ").strip()
+
+
+if len(ch) != 1:
+    print("Error: Please enter exactly one character.")
+else:
+
+    if ch.lower() in 'aeiou':
+        print(f"'{ch}' is a vowel.")
+    else:
+        print(f"'{ch}' is not a vowel.")
+
+print("≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈")
+
+print("End of Program..")
