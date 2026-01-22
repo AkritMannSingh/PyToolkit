c285007ea86da5cfb3e8f4d5eaee592bb07ed8f4
Akrit Mann Singh <akritmannsingh@gmail.com>
2026-01-22 16:06:42 +0530

    Delete pass_fail.py

--- pass_fail.py
+++
@@ -1,9 +0,0 @@
-passing_score = 50
-
-my_score = int(input("Enter your score:"))
-
-if (my_score>=passing_score):
-    print("Congratulations on passing the exam.")
-
-else:
-    print("You failed, try harder next time.")
